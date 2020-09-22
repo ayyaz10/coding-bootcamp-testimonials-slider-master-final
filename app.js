a266397ad7e3562022d468757614f6b23f41ee7f
codementee <codementee@outlook.com>
2020-09-22 13:44:32 +0500

    added sliding functionality

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-const testmonials = document.querySelectorAll('testmonial');
-console.log(testmonials)
+const testmonial = document.querySelectorAll(".testmonial");
+const slideLeft = document.querySelectorAll(".slide-left");
+const slideRight = document.querySelectorAll(".slide-right");
+
+for (let i = 1; i < testmonial.length; i++){
+    testmonial[i].classList.add("hide");
+}
+
+for (let i = 0; i < slideLeft.length; i++){
+    slideLeft[i].addEventListener("click", function(){
+        testmonial[0].classList.toggle("hide");
+        testmonial[1].classList.toggle("hide");
+    });
+}
+
+for (let i = 0; i < slideRight.length; i++){
+    slideRight[i].addEventListener("click", function(){
+        testmonial[0].classList.toggle("hide");
+        testmonial[1].classList.toggle("hide");
+    });
+}
